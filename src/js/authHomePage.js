269a508eb94ebc0c5af303a56d8d4d6dacb064b2
Shahriar Hridoy <multi.hridoy@gmail.com>
2020-01-17 20:09:33 +0600

    remove chat list option

--- src/js/authHomePage.js
+++ src/js/authHomePage.js
@@ -1,7 +1,6 @@
 // friendListOption
 
 const friendTab = document.querySelector('#nav-friends-tab');
-const chatListTab = document.querySelector('#nav-chatList-tab');
 
 const friendTabContent = document.querySelector('#nav-friends');
 const chatListTabContent = document.querySelector('#nav-chatList');
@@ -10,21 +9,13 @@ friendTab.addEventListener('click', e => {
   localStorage.setItem('chatState', 'friendsList');
 });
 
-chatListTab.addEventListener('click', e => {
-  localStorage.setItem('chatState', 'chatList');
-});
-
 // check every time at homepage
 if (localStorage.getItem('chatState') === 'chatList') {
   friendTab.classList.remove('active');
-  chatListTab.classList.add('active');
 
   friendTabContent.classList.remove('show', 'active');
-  chatListTabContent.classList.add('show', 'active');
 } else {
-  chatListTab.classList.remove('active');
   friendTab.classList.add('active');
 
-  chatListTabContent.classList.remove('show', 'active');
   friendTabContent.classList.add('show', 'active');
 }
