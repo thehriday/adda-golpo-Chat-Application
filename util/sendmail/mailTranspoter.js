e05802cbb9f68404614d3d8b183590462f7e4105
Shahriar Hridoy <multi.hridoy@gmail.com>
2020-01-25 22:48:05 +0600

    sendgrid setup complite

--- util/sendmail/mailTranspoter.js
+++ util/sendmail/mailTranspoter.js
@@ -4,9 +4,7 @@ const nodeMailerSendGridTransport = require('nodemailer-sendgrid-transport');
 module.exports = nodemailer.createTransport(
   nodeMailerSendGridTransport({
     auth: {
-      api_key:
-        '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',
-      api_user: 'mshidoy'
+      api_key: process.env.MAIL_API_KEY
     }
   })
 );
